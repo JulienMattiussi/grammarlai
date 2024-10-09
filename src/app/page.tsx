79929fc0b4e6cd6d9e708b06a2c73500d4191ec8
erwanMarmelab <erwan@marmelab.com>
2024-10-09 14:52:52 +0200

    make it (a bit) beautiful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
 import { useState } from "react";
 import { Result } from "./types";
 import { Input } from "./components/Input";
-import { Fixed } from "./components/Fixed";
 import { Sidebar } from "./components/Sidebar";
 import clsx from "clsx";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [text, setText] = useState<string>(
     "Hello im anthony and i like patatoes. wat about you ? I love doing horse. I think he is beautifull"
   );
@@ -15,23 +15,23 @@ export default function Home() {
     <div className="flex column">
       <div
         className={clsx(
-          "grow-[2] h-screen overflow-y-scroll",
+          "w-8/12 h-screen",
           "grid grid-rows-[20px_1fr_20px] items-center justify-items-center",
           "font-[family-name:var(--font-geist-sans)]"
         )}
       >
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start  w-8/12">
           <Input
             text={text}
             setText={setText}
             result={result}
             setResult={setResult}
+            isLoading={isLoading}
+            setIsLoading={setIsLoading}
           />
-          <Fixed text={result?.textSourceMarked} />
-          <Fixed text={result?.textTargetMarked} />
         </main>
       </div>
-      <div className="grow h-screen overflow-y-scroll">
+      <div className="w-4/12 h-screen overflow-y-scroll">
         <Sidebar result={result} setResult={setResult} />
       </div>
     </div>
